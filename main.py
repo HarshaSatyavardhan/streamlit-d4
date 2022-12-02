db8ee13f8eef974d05990ba934d41b31b332ebc4
specialized boy <41695878+HarshaSatyavardhan@users.noreply.github.com>
2022-12-02 22:55:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ solvent = form.text_input("Enter Solvent")
 submit_button = form.form_submit_button("Predict")
 
 if submit_button == True:
-    response = requests.get(f"https://d4-cigin.herokuapp.com/prediction?solute={solute}&solvent={solvent}")
+    response = requests.get(f"https://cigin.onrender.com/prediction?solute={solute}&solvent={solvent}")
     json = json.loads(response.text)
     st.metric("SOLVATION FREE ENERGY",json["prediction"]["solvation"])
     df = pd.DataFrame(json["prediction"]["interaction_map"])
